28586bf147be4f668685cc89a525fe30d7f3a85e
lwaziT <61688164+lwaziT@users.noreply.github.com>
2023-12-20 13:09:40 +0200

    Created Model

---
+++ src/main/java/com/example/crudWebApp/models/Institution.java
@@ -0,0 +1,33 @@
+package com.example.crudWebApp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "institution")
+public class Institution {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
